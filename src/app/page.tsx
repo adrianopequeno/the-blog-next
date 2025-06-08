a791be05eb54041a0cafda0a951fb54ec94e755a
Adriano Pequeno <adrian.pekeno@gmail.com>
2025-06-08 18:59:14 -0300

    feat: general container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import SpinLoader from '@/components/SpinLoader';
 
 const HomePage = async () => {
   return (
-    <div>
+    <div className="text-slate-900 bg-slate-300 min-h-screen dark:bg-slate-900 dark:text-slate-100">
       <h1>Título da página</h1>
       <Suspense fallback={<SpinLoader />}>
         <PostList />
