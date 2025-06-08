66dd077fb362138b802ee40d00dbf2160793f30a
Adriano Pequeno <adrian.pekeno@gmail.com>
2025-06-08 17:55:40 -0300

    feat: creating SpinLoader component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,10 @@
+import SpinLoader from '@/components/SpinLoader';
 import clsx from 'clsx';
 
 const HomePage = () => {
   return (
     <div>
-      <h1
-        className={clsx(
-          'text-xl',
-          'font-bold',
-          'text-blue-500',
-          'hover:text-white',
-          'hover:bg-blue-500',
-          'transition',
-          'duration-300'
-        )}
-      >
-        Texto H1
-      </h1>
+      <SpinLoader containerClasses={clsx('min-h-[500px]', 'bg-amber-500')} />
     </div>
   );
 };
