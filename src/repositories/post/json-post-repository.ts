573bc1b8c987513726f2e45ef6c8b0accae13f9e
Adriano Pequeno <adrian.pekeno@gmail.com>
2025-06-11 15:39:39 -0300

    chore: adjusted configuration to simulate data loading

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -12,7 +12,7 @@ const JSON_POSTS_FILE_PATH = resolve(
   'posts.json'
 );
 
-const SIMULATE_WAIT_IN_MS = 0;
+const SIMULATE_WAIT_IN_MS = 5000;
 
 export class JsonPostRepository implements PostRepository {
   private async simulateWait() {
@@ -40,8 +40,6 @@ export class JsonPostRepository implements PostRepository {
   }
 
   async findById(id: string): Promise<PostModel> {
-    await this.simulateWait();
-
     const posts = await this.findAllPublic();
     const post = posts.find((post: PostModel) => post.id === id);
     if (!post) {
@@ -51,8 +49,6 @@ export class JsonPostRepository implements PostRepository {
   }
 
   async findBySlug(slug: string): Promise<PostModel> {
-    await this.simulateWait();
-
     const posts = await this.findAllPublic();
     const post = posts.find((post: PostModel) => post.slug === slug);
     if (!post) {
