84b71cfb20c625342dd5114ed84ad25d05b6bc28
Adriano Pequeno <adrian.pekeno@gmail.com>
2025-06-11 15:01:24 -0300

    feat: generateMetadata - generating title, description or other metas dynamically - fixed title in not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,23 +2,26 @@ import clsx from 'clsx';
 
 const NotFount = () => {
   return (
-    <div
-      className={clsx(
-        'min-h-[320px] bg-slate-900 text-slate-100',
-        'mb-16 p-8 rounded-xl',
-        'flex items-center justify-center',
-        'text-center',
-        'dark:bg-slate-300 dark:text-slate-900'
-      )}
-    >
-      <div>
-        <h1 className="text-7xl/tight mb-4 font-extrabold">404 😱</h1>
-        <p>
-          Erro 404 - A página que você está tentando acessar não existe neste
-          site.
-        </p>
+    <>
+      <title>Página não encontrada</title>
+      <div
+        className={clsx(
+          'min-h-[320px] bg-slate-900 text-slate-100',
+          'mb-16 p-8 rounded-xl',
+          'flex items-center justify-center',
+          'text-center',
+          'dark:bg-slate-300 dark:text-slate-900'
+        )}
+      >
+        <div>
+          <h1 className="text-7xl/tight mb-4 font-extrabold">404 😱</h1>
+          <p>
+            Erro 404 - A página que você está tentando acessar não existe neste
+            site.
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
