92e5b3d02825cbc13e382008871b43be995412f9
Adriano Pequeno <adrian.pekeno@gmail.com>
2025-06-11 20:59:07 -0300

    feat: Tailwind Typography setup and HTML customization in React Markdown

--- src/components/SafeMarkdown/index.tsx
+++ src/components/SafeMarkdown/index.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import Markdown from 'react-markdown';
 import rehypeSanitize from 'rehype-sanitize';
 import remarkGfm from 'remark-gfm';
@@ -8,8 +9,45 @@ type SafeMarkdownProps = {
 
 export const SafeMarkdown = ({ markdown }: SafeMarkdownProps) => {
   return (
-    <div>
-      <Markdown rehypePlugins={[rehypeSanitize]} remarkPlugins={[remarkGfm]}>
+    <div
+      className={clsx(
+        'prose prose-slate',
+        'w-full max-w-none',
+        'overflow-hidden',
+        'prose-a:transition',
+        'prose-a:no-underline',
+        'prose-a:text-blue-500',
+        'prose-a:hover:text-blue-700',
+        'prose-a:hover:underline',
+        'prose-img:mx-auto',
+        'dark:prose-p:text-slate-100',
+        'dark:prose-h1:text-slate-100',
+        'dark:prose-h2:text-slate-100',
+        'dark:prose-h3:text-slate-100',
+        'dark:prose-strong:text-slate-100',
+        'dark:prose-li:text-slate-100',
+        'dark:prose-th:text-slate-100',
+        'dark:prose-tr:text-slate-100',
+        'dark:prose-td:text-slate-500',
+        'dark:prose-code:text-slate-500',
+        'md:prose-lg'
+      )}
+    >
+      <Markdown
+        rehypePlugins={[rehypeSanitize]}
+        remarkPlugins={[remarkGfm]}
+        components={{
+          table: ({ node, ...props }) => {
+            if (!node?.children) return '';
+
+            return (
+              <div className="overflow-x-auto">
+                <table className="w-full min-w-[600px]" {...props} />
+              </div>
+            );
+          },
+        }}
+      >
         {markdown}
       </Markdown>
     </div>
