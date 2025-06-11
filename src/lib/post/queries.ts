51e597925a0d2b971a1788fedee4973f438b360f
Adriano Pequeno <adrian.pekeno@gmail.com>
2025-06-11 14:42:42 -0300

    chore: improving findPostBySlugCached function for reuse

--- src/lib/post/queries.ts
+++ src/lib/post/queries.ts
@@ -1,5 +1,6 @@
 import { cache } from 'react';
 import { postRepository } from '@/repositories/post';
+import { notFound } from 'next/navigation';
 
 export const findAllPublicPostsCached = cache(
   async () => await postRepository.findAllPublic()
@@ -9,6 +10,10 @@ export const findPostByIdCached = cache(
   async (id: string) => await postRepository.findBySlug(id)
 );
 
-export const findPostBySlugCached = cache(
-  async (slug: string) => await postRepository.findBySlug(slug)
-);
+export const findPostBySlugCached = cache(async (slug: string) => {
+  const post = await postRepository.findBySlug(slug).catch(() => undefined);
+
+  if (!post) notFound();
+
+  return post;
+});
