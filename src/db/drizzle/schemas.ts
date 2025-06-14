76aff897d8edb53f39ad791265e25b7a9f96008a
Adriano Pequeno <adrian.pekeno@gmail.com>
2025-06-14 20:10:57 -0300

    chore: Drizzle ORM connection + migrations to manage database tables

--- src/db/drizzle/schemas.ts
+++ src/db/drizzle/schemas.ts
@@ -1,7 +1,7 @@
 import { InferInsertModel, InferSelectModel } from 'drizzle-orm';
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
-export const postTable = sqliteTable('posts', {
+export const postsTable = sqliteTable('posts', {
   id: text('id').primaryKey(),
   slug: text('slug').notNull().unique(),
   title: text('title').notNull(),
@@ -14,5 +14,5 @@ export const postTable = sqliteTable('posts', {
   updatedAt: text('updated_at').notNull(),
 });
 
-export type PostTableSelectMode = InferSelectModel<typeof postTable>;
-export type PostTableInsertMode = InferInsertModel<typeof postTable>;
+export type PostTableSelectMode = InferSelectModel<typeof postsTable>;
+export type PostTableInsertMode = InferInsertModel<typeof postsTable>;
