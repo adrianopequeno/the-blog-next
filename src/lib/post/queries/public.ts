d4513c439bccddf8181dc0722e737fcea2e65339
Adriano Pequeno <adrian.pekeno@gmail.com>
2025-07-16 17:27:28 -0300

    fix: cache bug fix + log function to separate our Next.js logs

--- src/lib/post/queries/public.ts
+++ src/lib/post/queries/public.ts
@@ -3,29 +3,34 @@ import { postRepository } from '@/repositories/post';
 import { notFound } from 'next/navigation';
 import { unstable_cache } from 'next/cache';
 
-export const findAllPublicPostsCached = unstable_cache(
-  cache(async () => {
-    return await postRepository.findAllPublic();
-  }),
-  ['posts'],
-  {
-    tags: ['posts'],
-  }
+export const findAllPublicPostsCached = cache(
+  unstable_cache(
+    async () => {
+      return await postRepository.findAllPublic();
+    },
+    ['posts'],
+    {
+      tags: ['posts'],
+    }
+  )
 );
 
-export const findPublicPostBySlugCached = (slug: string) =>
-  unstable_cache(
-    cache(async (slug: string) => {
+export const findPublicPostBySlugCached = cache((slug: string) => {
+  return unstable_cache(
+    async (slug: string) => {
       const post = await postRepository
         .findBySlugPublic(slug)
         .catch(() => undefined);
 
-      if (!post) notFound();
+      if (!post) {
+        notFound();
+      }
 
       return post;
-    }),
-    ['posts'],
+    },
+    [`post-${slug}`],
     {
       tags: [`post-${slug}`],
     }
   )(slug);
+});
