880a0844e0a97d279943610e540f9def1fd52e3f
Adriano Pequeno <adrian.pekeno@gmail.com>
2025-07-24 14:41:15 -0300

    feat: Dialog: Adding Component Functionality

--- src/components/PostListAdmin/index.tsx
+++ src/components/PostListAdmin/index.tsx
@@ -30,50 +30,6 @@ export default async function PostListAdmin() {
           </div>
         );
       })}
-
-      <div
-        className={clsx(
-          'fixed z-50 inset-0 bg-black/50 backdrop-blur-xs',
-          'flex items-center justify-center'
-        )}
-      >
-        <div
-          className={clsx(
-            'bg-slate-100 p-6 rounded-lg max-w-2xl mx-6',
-            'flex flex-col gap-6',
-            'shadow-lg shadow-black/40 text-center'
-          )}
-        >
-          <h3 className="text-xl font-extrabold">Título do diálogo</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
-            eveniet. Ipsum, commodi ipsam, dignissimos, deleniti ipsa nemo
-            corrupti molestias libero rerum aliquid ratione repudiandae! Sed
-            sequi itaque culpa dolore iusto.
-          </p>
-          <div className="flex items-center justify-around">
-            <button
-              className={clsx(
-                'bg-slate-300 hover:bg-slate-400 transition text-slate-950 rounded-lg',
-                'flex items-center justify-center',
-                'py-2 px-4 cursor-pointer'
-              )}
-              autoFocus
-            >
-              Cancelar
-            </button>
-            <button
-              className={clsx(
-                'bg-blue-500 hover:bg-blue-600 transition text-blue-50 rounded-lg',
-                'flex items-center justify-center',
-                'py-2 px-4 cursor-pointer'
-              )}
-            >
-              Ok
-            </button>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
--- src/components/admin/DeletePostButton/index.tsx
+++ src/components/admin/DeletePostButton/index.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { useTransition } from 'react';
+import { useState, useTransition } from 'react';
 import clsx from 'clsx';
 import { Trash2Icon } from 'lucide-react';
 
 import { deletePostAction } from '@/actions/post/delete-post-action';
+import { Dialog } from '@/components/Dialog';
 
 type DeletePostButtonProps = {
   id: string;
@@ -13,29 +14,47 @@ type DeletePostButtonProps = {
 
 export function DeletePostButton({ id, title }: DeletePostButtonProps) {
   const [isPending, startTransition] = useTransition();
+  const [showDialog, setShowDialog] = useState(false);
 
   function handleClick() {
-    if (!confirm('Tem certeza?')) return;
+    setShowDialog(true);
+  }
 
+  function handleConfirm() {
     startTransition(async () => {
       const result = await deletePostAction(id);
       alert(`O result é: ${result}`);
+      setShowDialog(false);
     });
   }
+
   return (
-    <button
-      className={clsx(
-        'text-red-500 cursor-pointer transition',
-        '[&_svg]:w-4 [&_svg]:h-4',
-        'hover:scale-120 hover:text-red-600',
-        'disabled:text-slate-600 disabled:cursor-not-allowed'
+    <>
+      <button
+        className={clsx(
+          'text-red-500 cursor-pointer transition',
+          '[&_svg]:w-4 [&_svg]:h-4',
+          'hover:scale-120 hover:text-red-600',
+          'disabled:text-slate-600 disabled:cursor-not-allowed'
+        )}
+        aria-label={`Apagar post: ${title}`}
+        title={`Apagar post: ${title}`}
+        onClick={handleClick}
+        disabled={isPending}
+      >
+        <Trash2Icon />
+      </button>
+
+      {showDialog && (
+        <Dialog
+          isVisible={showDialog}
+          title="Apagar Post?"
+          content={`Tem ceteza que deseja apagar o post: ${title}`}
+          onCancel={() => setShowDialog(false)}
+          onConfirm={handleConfirm}
+          disabled={isPending}
+        />
       )}
-      aria-label={`Apagar post: ${title}`}
-      title={`Apagar post: ${title}`}
-      onClick={handleClick}
-      disabled={isPending}
-    >
-      <Trash2Icon />
-    </button>
+    </>
   );
 }
