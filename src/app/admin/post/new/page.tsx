1f3686b956f10e74c9d0f8f731c4b19a8ce3d345
Adriano Pequeno <adrian.pekeno@gmail.com>
2025-07-28 21:12:29 -0300

    feat: concluding the Button component and its variations

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,13 +1,85 @@
 import { Button } from '@/components/Button';
+import { BanIcon, BugIcon, CheckIcon } from 'lucide-react';
 
 export const dynamic = 'force-dynamic';
 
 export default async function AdminNewPostPage() {
   return (
-    <div className="py-16 flex gap-4 flex-wrap">
-      <Button variant="default">Confimar</Button>
-      <Button variant="ghost">Cancelar</Button>
-      <Button variant="danger">Ok</Button>
+    <div>
+      <div className="py-16 flex gap-4 flex-wrap items-center">
+        <Button variant="default" size="sm" positionIcon="left">
+          <BugIcon />
+          Confimar
+        </Button>
+        <Button variant="ghost" size="md" positionIcon="top">
+          <BugIcon />
+          Cancelar
+        </Button>
+        <Button variant="danger" size="lg" positionIcon="right">
+          <BugIcon />
+          Ok
+        </Button>
+
+        <Button
+          variant="danger"
+          size="lg"
+          positionIcon="left"
+          elementPosition="center"
+          className="w-full"
+        >
+          <BugIcon />
+          Confimar
+        </Button>
+
+        <Button
+          variant="ghost"
+          size="lg"
+          positionIcon="right"
+          elementPosition="center"
+          className="w-full"
+        >
+          <BanIcon />
+          Cancel
+        </Button>
+
+        <Button
+          variant="default"
+          size="lg"
+          positionIcon="left"
+          elementPosition="center"
+          className="w-full"
+        >
+          <CheckIcon />
+          Ok
+        </Button>
+      </div>
+
+      <div className="py-16 flex gap-4 flex-wrap items-center">
+        <Button variant="default" size="sm" positionIcon="left" disabled>
+          <BugIcon />
+          Confimar
+        </Button>
+        <Button variant="ghost" size="md" positionIcon="top" disabled>
+          <BugIcon />
+          Cancelar
+        </Button>
+        <Button variant="danger" size="lg" positionIcon="right" disabled>
+          <BugIcon />
+          Ok
+        </Button>
+
+        <Button
+          variant="default"
+          size="md"
+          positionIcon="left"
+          elementPosition="center"
+          className="w-full"
+          disabled
+        >
+          <BugIcon />
+          Confimar
+        </Button>
+      </div>
     </div>
   );
 }
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,30 +2,76 @@ import clsx from 'clsx';
 
 type ButtonVariants = 'default' | 'ghost' | 'danger';
 type ButtonSizes = 'sm' | 'md' | 'lg';
+type ButtonIconPosition = 'left' | 'top' | 'right';
+type ButtonElementsPosition = 'left' | 'center' | 'right';
 
-type ButtonPrpos = {
+type ButtonProps = {
   variant?: ButtonVariants;
   size?: ButtonSizes;
+  positionIcon?: ButtonIconPosition;
+  elementPosition?: ButtonElementsPosition;
 } & React.ComponentProps<'button'>;
 
 export function Button({
   variant = 'default',
   size = 'md',
+  positionIcon = 'left',
+  elementPosition = 'center',
   ...props
-}: ButtonPrpos) {
+}: ButtonProps) {
   const buttonVariants: Record<ButtonVariants, string> = {
-    default: clsx('bg-blue-500 text-blue-100'),
-    ghost: clsx('bg-slate-200 text-slate-900'),
-    danger: clsx('bg-red-600 text-red-100'),
+    default: clsx('bg-blue-500 text-blue-100 hover:bg-blue-700'),
+    ghost: clsx('bg-slate-400 text-slate-900 hover:bg-slate-500'),
+    danger: clsx('bg-red-600 text-red-100 hover:bg-red-700'),
   };
 
   const buttonSizes: Record<ButtonSizes, string> = {
-    sm: clsx(),
-    md: clsx(),
-    lg: clsx(),
+    sm: clsx(
+      'text-xs/tight',
+      'py-1',
+      'px-2',
+      'rounded-sm',
+      '[&_svg]:w-3 [&_svg]:h-3'
+    ),
+    md: clsx(
+      'text-base/tight',
+      'py-2',
+      'px-4',
+      'rounded-md',
+      '[&_svg]:w-4 [&_svg]:h-4'
+    ),
+    lg: clsx(
+      'text-lg/tight',
+      'py-4',
+      'px-6',
+      'rounded-lg',
+      '[&_svg]:w-5 [&_svg]:h-5'
+    ),
   };
 
-  const buttonClasses = clsx(buttonVariants[variant], buttonSizes[size]);
+  const buttonIconPosition: Record<ButtonIconPosition, string> = {
+    left: clsx('flex-row gap-1'),
+    top: clsx('flex-col gap-2'),
+    right: clsx('flex-row-reverse gap-3'),
+  };
 
-  return <button className={buttonClasses} {...props} />;
+  const buttonElementsPosition: Record<ButtonElementsPosition, string> = {
+    left: clsx('justify-start'),
+    center: clsx('justify-center'),
+    right: clsx('justify-end'),
+  };
+
+  const buttonClasses = clsx(
+    buttonVariants[variant],
+    buttonSizes[size],
+    buttonIconPosition[positionIcon],
+    buttonElementsPosition[elementPosition],
+    'flex items-center',
+    'cursor-pointer',
+    'transition',
+    'disabled:bg-slate-200 disabled:text-slate-500 disabled:cursor-not-allowed',
+    props.className
+  );
+
+  return <button {...props} className={buttonClasses} />;
 }
