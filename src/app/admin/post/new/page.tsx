6ca63f0717674df596adbb658dec928b7ab25481
Adriano Pequeno <adrian.pekeno@gmail.com>
2025-08-05 15:21:43 -0300

    feat: creating the InputText component for the form

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,85 +1,34 @@
-import { Button } from '@/components/Button';
-import { BanIcon, BugIcon, CheckIcon } from 'lucide-react';
+import { InputText } from '@/components/InputText';
 
 export const dynamic = 'force-dynamic';
 
 export default async function AdminNewPostPage() {
   return (
-    <div>
-      <div className="py-16 flex gap-4 flex-wrap items-center">
-        <Button variant="default" size="sm" positionIcon="left">
-          <BugIcon />
-          Confimar
-        </Button>
-        <Button variant="ghost" size="md" positionIcon="top">
-          <BugIcon />
-          Cancelar
-        </Button>
-        <Button variant="danger" size="lg" positionIcon="right">
-          <BugIcon />
-          Ok
-        </Button>
-
-        <Button
-          variant="danger"
-          size="lg"
-          positionIcon="left"
-          elementPosition="center"
-          className="w-full"
-        >
-          <BugIcon />
-          Confimar
-        </Button>
-
-        <Button
-          variant="ghost"
-          size="lg"
-          positionIcon="right"
-          elementPosition="center"
-          className="w-full"
-        >
-          <BanIcon />
-          Cancel
-        </Button>
-
-        <Button
-          variant="default"
-          size="lg"
-          positionIcon="left"
-          elementPosition="center"
-          className="w-full"
-        >
-          <CheckIcon />
-          Ok
-        </Button>
-      </div>
-
-      <div className="py-16 flex gap-4 flex-wrap items-center">
-        <Button variant="default" size="sm" positionIcon="left" disabled>
-          <BugIcon />
-          Confimar
-        </Button>
-        <Button variant="ghost" size="md" positionIcon="top" disabled>
-          <BugIcon />
-          Cancelar
-        </Button>
-        <Button variant="danger" size="lg" positionIcon="right" disabled>
-          <BugIcon />
-          Ok
-        </Button>
-
-        <Button
-          variant="default"
-          size="md"
-          positionIcon="left"
-          elementPosition="center"
-          className="w-full"
-          disabled
-        >
-          <BugIcon />
-          Confimar
-        </Button>
-      </div>
+    <div className="flex flex-col gap-6">
+      <InputText labelText="Nome" placeholder="Digite seu nome" />
+      <InputText labelText="Sobrenome" placeholder="Digite seu sobrenome" />
+      <InputText
+        labelText="Sobrenome"
+        placeholder="Digite seu sobrenome"
+        disabled
+      />
+      <InputText
+        labelText="Sobrenome"
+        placeholder="Digite seu sobrenome"
+        disabled
+        defaultValue={'Olá Mundo!'}
+      />
+      <InputText
+        labelText="Sobrenome"
+        placeholder="Digite seu sobrenome"
+        defaultValue={'Olá Mundo!'}
+        readOnly
+      />
+      <InputText
+        labelText="Sobrenome"
+        placeholder="Digite seu sobrenome"
+        readOnly
+      />
     </div>
   );
 }
