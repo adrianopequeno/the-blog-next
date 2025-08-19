7494e65d16cafe41809cc5439ec5c7fdcb81a516
Adriano Pequeno <adrian.pekeno@gmail.com>
2025-08-18 21:42:16 -0300

    chore: understanding environment variables and .env.local

--- src/actions/upload/upload-image-action.ts
+++ src/actions/upload/upload-image-action.ts
@@ -1,10 +1,5 @@
 'use server';
 
-import {
-  IMAGE_SERVER_URL,
-  IMAGE_UPLOAD_DIRECTORY,
-  IMAGE_UPLOADER_MAX_SIZE,
-} from '@/lib/constants';
 import { mkdir, writeFile } from 'fs/promises';
 import { extname, resolve } from 'path';
 
@@ -29,7 +24,9 @@ export async function uploadImageAction(
     return makeResult({ error: 'Arquivo inválido' });
   }
 
-  if (file.size > IMAGE_UPLOADER_MAX_SIZE) {
+  const uploadMaxSize =
+    Number(process.env.NEXT_PUBLIC_IMAGE_UPLOADER_MAX_SIZE) || 921600;
+  if (file.size > uploadMaxSize) {
     return makeResult({ error: 'Arquivo muito grande' });
   }
 
@@ -43,11 +40,8 @@ export async function uploadImageAction(
   }
   const uniqueImageName = `${Date.now()}${imageExtension}`;
 
-  const uploadFullPath = resolve(
-    process.cwd(),
-    'public',
-    IMAGE_UPLOAD_DIRECTORY
-  );
+  const imageUploadDirectory = process.env.IMAGE_UPLOAD_DIRECTORY || 'uploads';
+  const uploadFullPath = resolve(process.cwd(), 'public', imageUploadDirectory);
   await mkdir(uploadFullPath, { recursive: true });
 
   const fileArrayBuffer = await file.arrayBuffer();
@@ -57,7 +51,9 @@ export async function uploadImageAction(
 
   await writeFile(fileFullPath, buffer);
 
-  const url = `${IMAGE_SERVER_URL}/${uniqueImageName}`;
+  const imageServerURL =
+    process.env.IMAGE_SERVER_URL || 'http://localhost:3000/uploads';
+  const url = `${imageServerURL}/${uniqueImageName}`;
 
   return makeResult({ url });
 }
--- src/components/admin/ImageUploader/index.tsx
+++ src/components/admin/ImageUploader/index.tsx
@@ -2,7 +2,6 @@
 
 import { uploadImageAction } from '@/actions/upload/upload-image-action';
 import { Button } from '@/components/Button';
-import { IMAGE_UPLOADER_MAX_SIZE } from '@/lib/constants';
 import { ImageUpIcon } from 'lucide-react';
 import { useRef, useState, useTransition } from 'react';
 import { toast } from 'react-toastify';
@@ -40,8 +39,10 @@ export function ImageUploader({ disabled }: ImageUploaderProps) {
       return;
     }
 
-    if (file.size > IMAGE_UPLOADER_MAX_SIZE) {
-      const readableMaxSize = IMAGE_UPLOADER_MAX_SIZE / 1024;
+    const uploadMaxSize =
+      Number(process.env.NEXT_PUBLIC_IMAGE_UPLOADER_MAX_SIZE) || 921600;
+    if (file.size > uploadMaxSize) {
+      const readableMaxSize = (uploadMaxSize / 1024).toFixed(2);
       toast.error(`O tamanho máximo permitido é de ${readableMaxSize}kbs`);
 
       fileInput.value = '';
